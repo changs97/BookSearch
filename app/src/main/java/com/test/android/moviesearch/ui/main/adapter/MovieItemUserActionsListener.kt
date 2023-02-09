e6c71305fb62c3af9c1e35dac237cdf964fa6057
Changhyung <pck4949@naver.com>
2023-02-09 18:29:13 +0900

    인터페이스 사용 이유가 없으므로 제거

--- app/src/main/java/com/test/android/moviesearch/ui/main/adapter/MovieItemUserActionsListener.kt
+++
@@ -1,8 +0,0 @@
-package com.test.android.moviesearch.ui.main.adapter
-
-import com.test.android.moviesearch.data.Movie
-
-
-interface MovieItemUserActionsListener {
-    fun onItemClicked(item: Movie)
-}
