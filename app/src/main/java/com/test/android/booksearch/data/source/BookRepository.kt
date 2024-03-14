dbd134d597fb3d44030edfca7bd654c1cd23605a
changs97 <kin2027@gmail.com>
2024-03-14 18:53:23 +0900

    Fix: repository에 service말고 apiHelper 주입 받도록 수정

--- app/src/main/java/com/test/android/booksearch/data/source/BookRepository.kt
+++ app/src/main/java/com/test/android/booksearch/data/source/BookRepository.kt
@@ -1,11 +1,11 @@
 package com.test.android.booksearch.data.source
 
-import com.test.android.booksearch.data.source.remote.BookService
+import com.test.android.booksearch.data.source.remote.ApiHelper
 import javax.inject.Inject
 
 
 class BookRepository @Inject constructor(
-    private val service: BookService
+    private val apiHelper: ApiHelper
 ) {
-    suspend fun getBooks(query: String) = service.getBooks(query)
+    suspend fun getBooks(query: String) = apiHelper.getBooks(query)
 }
